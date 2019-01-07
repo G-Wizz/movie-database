27bf7f0f3b7e5456c5c0837cc647620875db5a49
Greg Schmitt <gwizzprg100@gmail.com>
2019-01-07 08:34:31 -0500

    updated w removed api key

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import MoviesList from "./MoviesList";
+import MovieDetail from "./MovieDetail";
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+          <h1 id="main-title">Movie Database</h1>
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
